412175eb098007452eb471b6e0a3e54c541a61a0
Lydia Hallie <lydiajuliettehallie@gmail.com>
2018-05-30 09:21:35 +0200

    Create first schema

---
+++ imports/startup/server/index.js
@@ -0,0 +1,23 @@
+import { createApolloServer } from 'meteor/apollo';
+import { makeExecutableSchema } from 'graphql-tools';
+
+const typeDefs = `
+type Query {
+  hi: String
+}
+`;
+
+const resolvers = {
+  Query: {
+    hi() {
+      return "Hey world!"
+    }
+  }
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+createApolloServer({ schema });
---
+++ imports/ui/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <h1>Test</h1>
+
+export default App;
